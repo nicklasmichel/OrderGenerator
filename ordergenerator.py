42c2df7c2fd8a4f7789b2a40d5f28a9e3d9ecb7d
Nicklas Michel <nicklas.michel@esforin.com>
2019-06-03 12:28:16 +0200

    'test2'

--- ordergenerator.py
+++ ordergenerator.py
@@ -1,2 +1,7 @@
-print('OrderGenerator')
-priknt('Nicklas Michel')
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
